b4a274e396e3a9994698556e9e1316aaad2040ff
GabiSBD <102363296+GabiSBD@users.noreply.github.com>
2022-07-01 11:16:09 +0200

    Initial commit

---
+++ src/EjercicioTema1/Ejercicio1.java
@@ -0,0 +1,23 @@
+package EjercicioTema1;
+
+public class Ejercicio1 {
+    public static void main(String[]args){
+        //Numericos
+        //1.1 Enteros
+        byte e1=10;
+        short e2=100;
+        int e3=2590;
+        long e4=15478569L;
+        //1.2 Decimales
+        float e5=1.55f;
+        double e6=55.6;
+        //Booleano
+        boolean v=true;
+        boolean f=false;
+        //Texto
+        char c='f';
+        String palabra="Hola";
+
+        System.out.println(e1+"\n"+e2+"\n"+e3+"\n"+e4+"\n"+e5+"\n"+e6+"\n"+v+"\n"+f+"\n"+c+"\n"+palabra);
+    }
+}
